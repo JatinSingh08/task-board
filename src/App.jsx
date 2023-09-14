21683849f8e3ce1ccda20b34de13a8026c21cbf0
JatinSingh08 <singhjatin0812@gmail.com>
2023-09-14 16:26:43 +0530

    ⚡initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-yellow-500 h-[100vh] items-center justify-center font-bold text-4xl flex'>
+      Hello world
+    </div>
+  )
+}
+
+export default App
